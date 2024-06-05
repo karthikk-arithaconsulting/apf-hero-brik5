3bb70c336cd9d33ed367d4fc5c14f9c9a8064c4a
karthik <karthik.k@arithaconsulting.com>
2024-06-05 12:30:10 +0530

    edit page break issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,44 @@
 import { ThemeContextProvider } from "@apf/core";
-import { AppWrapper } from "@apf/app-wrapper";
+import { AppWrapper } from "@apf/app-wrapper2";
 import "@apf/core/dist/style.css";
 import "./App.css";
 import { useState } from "react";
+// import InputData from "./data/schema.json";
 import InputData from "./data/schema.json";
 import React from "react";
 import HeroBrikFive from "./components/HeroBrikFive";
+import { BrikWrapper } from "@apf/brik-wrapper";
 
 function App() {
-  const [brikData, setBrikData] = useState(InputData);
-  const resetJson = () => setBrikData(InputData);
+  const [data, setData] = useState(JSON.parse(JSON.stringify(InputData)));
+  const [brikId, setBrikId] = useState("");
+  const [showEditor, setShowEditor] = useState(false);
+
+  const handleBrikSelect = (id) => {
+    setBrikId(id);
+    setShowEditor(true);
+  };
 
   return (
     <ThemeContextProvider>
       <AppWrapper
-        brikData={brikData}
-        resetJson={resetJson}
-        handleChange={(value) => setBrikData(value)}
+        data={data}
+        setData={setData}
+        defaultData={InputData}
+        showEditor={showEditor}
+        handleClose={() => setShowEditor(false)}
+        isBrik={true}
+        brikId={brikId}
       >
         <div className="App">
-          <HeroBrikFive data={brikData}/>
+          <BrikWrapper
+            handleClick={handleBrikSelect}
+            data={data?.pages[0]?.pageData[0]?.cols[0]?.colData[0]}
+          >
+            <HeroBrikFive
+              data={data?.pages[0]?.pageData[0]?.cols[0]?.colData[0]}
+            />
+          </BrikWrapper>
         </div>
       </AppWrapper>
     </ThemeContextProvider>
